bae14c8341ed130d3feb2ecabe71da73fba09b3e
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-06-28 16:35:30 -0500

    remove old news component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import '../styles/globals.scss'
-import News from './news'
 
 export default function Home() {
   return (
     <main>
       <h1>This is 'page' content</h1>
-      {/* @ts-expect-error Async Server Component */}
-      <News/>
     </main>
   )
 }
