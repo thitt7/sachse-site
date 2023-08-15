9f7cff0ecabbf57ddf272e615c23c4b6c0312682
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-08-15 01:22:07 -0500

    updated calendar event modals with URL parameters

--- src/app/events/eventModal.tsx
+++ src/app/events/eventModal.tsx
@@ -1,70 +1,92 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 import getEvents from '@/lib/getEvents';
 
 type Event = {
-    title?: string,
-    slug?: string,
-    location?: string,
-    address?: string,
-    description?: {html: string, text: string},
-    start?: Date,
-    end?: Date,
-    URL?: string,
-    img?: {src: string, alt: string},
-    allDay?: boolean
+  title?: string,
+  slug?: string,
+  location?: string,
+  address?: string,
+  description?: { html: string, text: string },
+  start?: Date,
+  end?: Date,
+  URL?: string,
+  img?: { src: string, alt: string },
+  allDay?: boolean
 }
 
 type Props = {
-    id: string,
-    isOpen: boolean
+  id: string,
+  isOpen: boolean
 }
 
-const EventModal = ({id, isOpen}: Props) => {
+const EventModal = ({ id, isOpen }: Props) => {
 
   const getEvent = useCallback(
     async () => {
-      const event = await fetch (`/api/events?id=${id}`)
+      const event = await fetch(`/api/events?id=${id}`)
       return await event.json()
     },
     [id]
   );
 
-  // const [eventID, setEventID] = useState<string>(id)
-  const [event, setEvent] = useState<Promise<Event>>(async ()=>{return getEvent()})
+  const router = useRouter()
+  const pathName = usePathname()
+  const searchParams = useSearchParams()!
+
+  const [eventID, setEventID] = useState<string>(id)
+  const [event, setEvent] = useState<Promise<Event>>()
   const [open, setOpen] = useState(isOpen)
 
+  const handleClose = () => {
+    console.log('closing')
+    setOpen(false);
+    removeParams()
+  };
+
+  const removeParams = () => {
+    router.push(pathName!)
+  }
+
   useEffect(() => {
+    console.log('running useeffect hook...')
     const fetchEvent = async () => {
       const event = await getEvent()
       console.log(event)
       setEvent(event[0])
     }
-    fetchEvent()
-  }, [])
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    if (id) {
+      console.log('ID IS NOT NULL')
+      fetchEvent()
+      setOpen(true)
+    }
+  }, [])
 
   console.log('OPEN STATE: ', open)
 
   return (
     <>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          
-        </DialogTitle>
-        <DialogContent>
-        Example Content
-        </DialogContent>
-       
-      </Dialog>
+      {
+        event ?
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            maxWidth='md'
+          >
+            <DialogTitle id="alert-dialog-title">
+              {event.title}
+            </DialogTitle>
+            <DialogContent>
+              {event.description ? <p dangerouslySetInnerHTML={{ __html: event.description.html }}></p> : ''}
+              Your cunt is too pixelated -_-
+            </DialogContent>
+
+          </Dialog> : ''
+      }
     </>
   )
 }
