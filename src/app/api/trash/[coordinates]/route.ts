b74b9c40fc59b037a97ad85c4f3f4ae80c92494e
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2023-11-13 00:55:59 -0600

    created separate maps API key for server-side

--- src/app/api/trash/[coordinates]/route.ts
+++ src/app/api/trash/[coordinates]/route.ts
@@ -4,12 +4,12 @@ export async function GET( request: Request, { params }: { params: { coordinates
     const coordinates = params.coordinates.toString();
     let split = coordinates!.split(',')
 
-    const result = await fetch (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${split[0]},${split[1]}&key=${process.env.GOOGLE_MAPS_API_KEY}`, { });
+    const result = await fetch (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${split[0]},${split[1]}&key=${process.env.GOOGLE_MAPS_API_KEY_SERVER}`);
     const response = await result.json()
+    console.log('RES: ', response)
     if (response.status == 'OK') {return NextResponse.json(response.results[0])}
     else {
         return new Response('Failed to retrieve trash data', { status: 500, headers: { 'error': response.error_message } })
     }
-    // console.log('STATUS: ', response.status)
 
   }
